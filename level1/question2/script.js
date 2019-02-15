4cfa7568cf75a01e778cce07980a501e7b9b63f8
P1wP <jokke90@gmail.com>
2019-02-15 13:20:08 +0100

    code level 1 complete

--- level1/question2/script.js
+++ level1/question2/script.js
@@ -1,1 +1,38 @@
 //Console.log out elements in JSON file
+
+// JSON
+var jsonObject = {
+	"video": [{
+		"id": 12312412312,
+		"name": "Ecuaciones Diferenciales",
+		"url": "/video/math/edo/12312412312",
+		"author": {
+			"data": [{
+				"name_author": "Alejandro Morales",
+				"url": "/author/alejandro-morales",
+				"type": "master"
+			}]
+		}
+	}]
+}
+
+//LOG ELEMENTS
+var output = "";
+var output2 = "";
+for (var i = 0; i < jsonObject.video.length && i < jsonObject.video[0].author.data.length; i++){
+    for (key in jsonObject.video[i]){                      
+            if (jsonObject.video[i].hasOwnProperty(key)) {
+                output += [key] +  ": " + jsonObject.video[i][key] + "\n"
+        }
+    }
+    for (key in jsonObject.video[0].author.data[i]){                      
+            if (jsonObject.video[0].author.data[i].hasOwnProperty(key)) {
+                output2 += [key] +  ": " + jsonObject.video[0].author.data[i][key] + "\n"
+        }
+    }
+}
+        
+ 
+console.log(output + output2);   
+
+
