d7111b826e4e4e8d6aaf342db698fdf26ad93bf6
P1wP <jokke90@gmail.com>
2019-02-15 10:13:13 +0100

    Lvl 1 Q 1 complete

--- level1/question1/script.js
+++ level1/question1/script.js
@@ -1,1 +1,16 @@
 //Use RegEx to validate form
+/*
+When you click the button, call a JavaScript function that should validate that the first number is 0 and the last number is 7. The number should only be 7 digits long. If it is not it should log to the console.
+*/
+
+document.getElementById("btn").addEventListener("click", function () {
+    var form = document.getElementsByTagName("input");
+    for (var i = 0; i < form.length; i++){
+        var myInput = form[i].value;
+        console.log(myInput);
+        var rules = new RegExp(form[i].pattern);
+        var theTest = rules.test(myInput);
+        console.log(theTest);
+    }
+});
+
